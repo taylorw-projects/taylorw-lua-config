c02e44620b2e6d56a5e67ed3e2b8559f4c27ecd2
Taylor Whitton <taylorw@knowbe4.com>
2022-09-04 19:06:40 -0400

    configuration changes

--- lua/modules/lsp/config.lua
+++ lua/modules/lsp/config.lua
@@ -1,23 +1,28 @@
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local telescope = require("telescope.builtin")
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
+vim.keymap.set("n", "<space>q", telescope.diagnostics, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
+---@diagnostic disable-next-line: unused-local
 local on_attach = function(client, bufnr)
 	-- Mappings.
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
 	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "gd", telescope.lsp_definitions, bufopts)
+    vim.keymap.set("n", "gt", telescope.lsp_type_definitions, bufopts)
+    vim.keymap.set("n", "gs", telescope.lsp_document_symbols, bufopts)
 	vim.keymap.set("n", "<space>i", vim.lsp.buf.hover, bufopts)
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("n", "gi", telescope.lsp_implementations, bufopts)
 	vim.keymap.set("n", "gv", ":vsplit<CR><C-w>l<CMD>lua vim.lsp.buf.definition()<CR>", bufopts)
 	vim.keymap.set("n", "gV", ":vsplit<CR><C-w>l<CMD>lua vim.lsp.buf.declaration()<CR>", bufopts)
+	vim.keymap.set("n", "gr", telescope.lsp_references, bufopts)
 	vim.keymap.set("n", "<space>I", vim.lsp.buf.signature_help, bufopts)
 	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
 	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
@@ -27,7 +32,6 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set("n", "<space>d", vim.lsp.buf.type_definition, bufopts)
 	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
 	vim.keymap.set("n", "<space>ca", ":CodeActionMenu<CR>", bufopts)
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
 	vim.keymap.set("n", "<space>f", vim.lsp.buf.formatting, bufopts)
 end
 
