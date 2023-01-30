15c810af132b976ffc3e2a6210cecb69d55fc374
Taylor Whitton <taylorw@knowbe4.com>
2023-01-30 12:56:11 -0500

    Go enable language server and update nvim-tree config for update

--- lua/modules/lsp/config.lua
+++ lua/modules/lsp/config.lua
@@ -3,61 +3,66 @@ M = {}
 M.setup = function()
     local on_attach = require('keybinds').lsp_on_attach
 
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
     local lsp_flags = {
         -- This is the default in Nvim 0.7+
         debounce_text_changes = 150,
     }
-    require("lspconfig")["jsonls"].setup({
+    require('lspconfig')['gopls'].setup({
         capabilities = capabilities,
         on_attach = on_attach,
         flags = lsp_flags,
     })
-    require("lspconfig")["pyright"].setup({
+    require('lspconfig')['jsonls'].setup({
         capabilities = capabilities,
         on_attach = on_attach,
         flags = lsp_flags,
     })
-    require("lspconfig")["tsserver"].setup({
+    require('lspconfig')['pyright'].setup({
         capabilities = capabilities,
         on_attach = on_attach,
         flags = lsp_flags,
     })
-    require("lspconfig")["sumneko_lua"].setup {
+    require('lspconfig')['tsserver'].setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        flags = lsp_flags,
+    })
+    require('lspconfig')['sumneko_lua'].setup {
         capabilities = capabilities,
         on_attach = on_attach,
         flags = lsp_flags,
         settings = {
             Lua = {
-                runtime = { version = "LuaJIT", path = vim.split(package.path, ';'), },
-                completion = { keywordSnippet = "Disable", },
+                runtime = { version = 'LuaJIT', path = vim.split(package.path, ';'), },
+                completion = { keywordSnippet = 'Disable', },
                 diagnostics = { enable = true, globals = {
-                    "vim", "describe", "it", "before_each", "after_each"
+                    'vim', 'describe', 'it', 'before_each', 'after_each'
                 },
                 },
                 workspace = {
                     library = {
-                        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                        [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+                        [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                        [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
                     }
                 }
             }
         }
     }
-    require("lspconfig")["terraform_lsp"].setup({
+    require('lspconfig')['terraform_lsp'].setup({
         capabilities = capabilities,
         on_attach = on_attach,
         flags = lsp_flags,
     })
-    require("lspconfig")["rust_analyzer"].setup({
+    require('lspconfig')['rust_analyzer'].setup({
         capabilities = capabilities,
         on_attach = on_attach,
         flags = lsp_flags,
         settings = {
-            ["rust-analyzer"] = {
+            ['rust-analyzer'] = {
                 checkOnSave = {
-                    command = "clippy",
+                    command = 'clippy',
                 }
             }
         }
--- lua/modules/utilities/nvim_tree.lua
+++ lua/modules/utilities/nvim_tree.lua
@@ -4,8 +4,6 @@ M = {}
 M.setup = function()
     require("nvim-tree").setup({
         sync_root_with_cwd = true,
-        open_on_setup = true,
-        open_on_setup_file = true,
         auto_reload_on_write = true,
     })
 end
